45ceebec318da6b4c28fdef58f5c0a989a091597
leonardom249 <leonardo.megan@gmail.com>
2018-08-20 21:23:28 -0500

    changed obj variable to show it doesnt need to just say obj, can say anything there and still work

--- MainClass.java
+++ MainClass.java
@@ -10,9 +10,9 @@ class Check{
   public class MainClass{
     public static void main(String[] args){
      int amt=0;
-     Check obj= new Check();
-     obj.setAmount(200);
-     amt=obj.getAmount(); 
+     Check obj_variable_name= new Check();
+     obj_variable_name.setAmount(200);
+     amt=obj_variable_name.getAmount(); 
      System.out.println("Your current amount is: "+amt);
      }
   }
