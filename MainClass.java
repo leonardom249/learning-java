695b6a16b5beee9e2a8335d2becd6a120d5bb29f
leonardom249 <leonardo.megan@gmail.com>
2018-08-20 21:21:14 -0500

    adding oop example for encapsulation

---
+++ MainClass.java
@@ -0,0 +1,18 @@
+class Check{
+    private int amount=0;
+    public int getAmount(){
+     return amount;
+    }
+    public void setAmount(int amt){
+     amount=amt;
+    } 
+  }
+  public class MainClass{
+    public static void main(String[] args){
+     int amt=0;
+     Check obj= new Check();
+     obj.setAmount(200);
+     amt=obj.getAmount(); 
+     System.out.println("Your current amount is: "+amt);
+     }
+  }
